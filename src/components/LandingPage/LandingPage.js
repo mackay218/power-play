b19260ee2bacb171e0c22b95c97a041d898dfc05
Daniel MacKay <mackay2588@gmail.com>
2018-10-11 12:27:40 -0500

    style layout changes to landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -35,54 +35,78 @@ class LandingPage extends Component {
                 <div className="missionContainer">
                     <h1 className="missionHeader">
                         A Mission Statement A Mission Statement A Mission Statement</h1>
-                    <br></br>
                     <h3 className="missionDetail">
                         We do the things. All the things. All the time.
                         We do the things. All the things. All the time.
                     We do the things. All the things. All the time.</h3>
-                    <br></br>
                     <h3 className="missionDetail">
                         And we do them well. We do them with gusto. We accomplish them with gravitas.
                     </h3>
                 </div>
                 <div className="guideContainer">
-
-                    <div className="guideSections">
-                        <h1 className="guideHeaders">Build Your Profile</h1>
+                    <div className="guideSection">
+                        <h3 className="guideHeaders">Build Your Profile</h3>
                         <p>Enter your stats.</p>
                     </div>
-                    {/* <br></br> */}
-                    <span></span>
-                    <div className="guideSections">
-                        <h1 className="guideHeaders">Showcase Your Talent</h1>
+                    <div className="guideSection">
+                        <h3 className="guideHeaders">Showcase Your Talent</h3>
                         <p>Load a clip of your skills in action.</p>
                     </div>
-                    {/* <br></br> */}
-                    <span></span>
-                    <div className="guideSections">
-                        <h1 className="guideHeaders">Get Noticed</h1>
+                    <div className="guideSection">
+                        <h3 className="guideHeaders">Get Noticed</h3>
                         <p>Your profile will be accessible to coaches across the country.</p>
                     </div>
                 </div>
 
                 <div className="testimonialContainer">
 
-                    <div className="testimonialSections">
-                    A testimonial of the greatness achieved by usage of this service.
-                    <br></br>
-                    An image of a fancy lad will appear above this.
+                    <div className="testimonialSection">
+                        <img src="https://eadb.org/wp-content/uploads/2015/08/profile-placeholder.jpg" alt="placeholder" ></img>
+                            <p>"Lorem ipsum dolor sit amet, 
+                                consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt 
+                                ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis 
+                                nostrud exercitation ullamco laboris 
+                                nisi ut aliquip ex ea commodo consequat. 
+                                Duis aute irure dolor in reprehenderit in 
+                                voluptate velit esse cillum dolore eu fugiat 
+                                nulla pariatur. Excepteur sint occaecat cupidatat 
+                                non proident, sunt in culpa qui officia deserunt 
+                                mollit anim id est laborum."
+                            </p>
                     </div>
-
-                    <div className="testimonialSections">
-                    A testimonial of the greatness achieved by usage of this service.
-                    <br></br>
-                    An image of a fancy lad will appear above this.
+                    <div className="testimonialSection">
+                        <img src="https://eadb.org/wp-content/uploads/2015/08/profile-placeholder.jpg" alt="placeholder" ></img>
+                        <p>"Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat
+                            nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt
+                            mollit anim id est laborum."
+                            </p>
                     </div>
-
-                    <div className="testimonialSections">
-                    A testimonial of the greatness achieved by usage of this service.
-                    <br></br>
-                    An image of a fancy lad will appear above this.
+                    <div className="testimonialSection">
+                        <img src="https://eadb.org/wp-content/uploads/2015/08/profile-placeholder.jpg" alt="placeholder" ></img>
+                        <p>"Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat
+                            nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt
+                            mollit anim id est laborum."
+                            </p>
                     </div>
 
                 </div>
