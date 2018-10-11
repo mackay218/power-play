688f69a35b095d698a8e407f796662b71ae7a794
Isaac Overlander <Isaac.overlander@gmail.com>
2018-10-11 12:44:26 -0500

    Added ban and suspend buttons

--- server/routes/coach.router.js
+++ server/routes/coach.router.js
@@ -12,7 +12,7 @@ const chance = new Chance();
  * GET route template
  */
 router.get('/all', (req, res) => {
-    const query = `SELECT "person"."id", "email", "coach_name", "status_type" FROM "person" JOIN "account_status" ON "status_id" = "account_status"."id" WHERE "role" = 'coach';`;
+    const query = `SELECT "person"."id", "email", "coach_name", "status_type" FROM "person" JOIN "account_status" ON "status_id" = "account_status"."id" WHERE "role" = 'coach' LIMIT 10;`;
     pool.query(query).then((result) => {
         console.log(result.rows);
         res.send(result.rows);
@@ -31,13 +31,27 @@ router.delete('/delete/:id', (req, res) => {
         console.log('ERROR deleting the coach', error);
         res.sendStatus(500);
     });
-})
+});
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-})
+router.put('/suspend/:id', (req, res) => {
+    const query = `UPDATE "person" SET "status_id" = 2 WHERE "id" = $1;`;
+    pool.query(query, [req.params.id]).then(() => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('ERROR suspending coach:', error);
+        res.sendStatus(500);
+    });
+});
+
+router.put('/ban/:id', (req, res) => {
+    const query = `UPDATE "person" SET "status_id" = 3 WHERE "id" = $1;`;
+    pool.query(query, [req.params.id]).then(() => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('ERROR banning coach:', error);
+        res.sendStatus(500);
+    });
+});
 
 //function to check if coach's email already in database
 router.post('/checkCoach', (req, res) => {
@@ -69,7 +83,6 @@ router.post('/checkCoach', (req, res) => {
             res.sendStatus(500);
         });
 });
-
 
 //function to add new coach to database
 //only called by Admin
