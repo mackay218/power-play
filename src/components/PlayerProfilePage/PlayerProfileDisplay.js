5f9132f77611358aca03e0d430e6836ca4f2872b
Daniel MacKay <mackay2588@gmail.com>
2018-10-16 10:46:46 -0500

    added contact page route and nav link

--- src/components/PlayerProfilePage/PlayerProfileDisplay.js
+++ src/components/PlayerProfilePage/PlayerProfileDisplay.js
@@ -12,10 +12,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
-<<<<<<< HEAD
 
-=======
->>>>>>> master
+
 import PlayerProfileDialog from './PlayerProfileDialog';
 
 
