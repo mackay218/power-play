c807ffafe655b67a9c1de82e0228c7b9ea4ffc86
Ahmed <omar0121@umn.edu>
2018-10-18 10:13:58 -0500

    highlight teams on hover

--- src/components/TeamsPage/TeamsPage.js
+++ src/components/TeamsPage/TeamsPage.js
@@ -58,70 +58,70 @@ class TeamsPage extends Component{
                         <div className="leagueDivision D1">
                             <h2>College D1</h2>
                             <ul>
-                                <li>Bemidji State Univeristy</li>
-                                <li>Northern Michigan University</li>
-                                <li>Western Michigan University</li>
-                                <li>American International College</li>
+                                <li className="team-list">Bemidji State Univeristy</li>
+                                <li className="team-list">Northern Michigan University</li>
+                                <li className="team-list">Western Michigan University</li>
+                                <li className="team-list">American International College</li>
                             </ul>
                         </div>
                         <div className="leagueDivision D3">
                             <h2>College D3</h2>
                             <ul>
-                                <li>Augsburg University</li>
-                                <li>Aurora University</li>
-                                <li>Bethel University</li>
-                                <li>Concordia College</li>
-                                <li>Gustavus Adolphus College</li>
-                                <li>St. John's University</li>
-                                <li>St. Mary's University</li>
-                                <li>St. Olaf College</li>
-                                <li>University of St. Thomas</li>
-                                <li>Marian University</li>
-                                <li>Trine University</li>
-                                <li>St. Scholastica</li>
-                                <li>University of Wisconsin Eau Claire</li>
-                                <li>Univeristy of Wisconsin Stout</li>
-                                <li>University of Wisconsin River Falls</li>
-                                <li>Finlandia University</li>
-                                <li></li>
+                                <li className="team-list">Augsburg University</li>
+                                <li className="team-list">Aurora University</li>
+                                <li className="team-list">Bethel University</li>
+                                <li className="team-list">Concordia College</li>
+                                <li className="team-list">Gustavus Adolphus College</li>
+                                <li className="team-list">St. John's University</li>
+                                <li className="team-list">St. Mary's University</li>
+                                <li className="team-list">St. Olaf College</li>
+                                <li className="team-list">University of St. Thomas</li>
+                                <li className="team-list">Marian University</li>
+                                <li className="team-list">Trine University</li>
+                                <li className="team-list">St. Scholastica</li>
+                                <li className="team-list">University of Wisconsin Eau Claire</li>
+                                <li className="team-list">Univeristy of Wisconsin Stout</li>
+                                <li className="team-list">University of Wisconsin River Falls</li>
+                                <li className="team-list">Finlandia University</li>
+                                <li className="team-list"></li>
                             </ul>
                         </div>
                         <div className="leagueDivision Juniors">
                             <h2>Juniors</h2>
                             <ul>
-                                <li>Aberdeen Wings</li>
-                                <li>Austin Bruins</li>
-                                <li>Chippewa Falls Steel</li>
-                                <li>Johnstown Tomahawks</li>
-                                <li>Kenai Brown Bears</li>
-                                <li>MN Magicians</li>
-                                <li>MN Wilderness</li>
-                                <li>Minot Minotauros</li>
-                                <li>NE Generals</li>
-                                <li>Brookings Blizzard</li>
-                                <li>Alexandria Blizzard</li>
-                                <li>Bozeman Icedogs</li>
-                                <li>Breezy Point Stars</li>
-                                <li>Granite City Lumberjacks</li>
-                                <li>New Ulm Steel</li>
-                                <li>North Iowa Bulls</li>
-                                <li>Rochester Grizzlies</li>
-                                <li>MN Blue Ox</li>
-                                <li>MN Mullets</li>
-                                <li>Rum River</li>
-                                <li>Steele County Blades</li>
-                                <li>Hudson Havoc</li>
-                                <li>Fort Frances Lakers</li>
-                                <li>Tampa Bay Jr. Lightning</li>
-                                <li>Oklahoma City Blazers</li>
-                                <li>Thief River Falls Norskies</li>
-                                <li>Swan Valley Stampeders</li>
-                                <li>Steinbach Pistons</li>
-                                <li>Winkler Flyers</li>
-                                <li>Bonnyville Pontiacs</li>
-                                <li>Green Bay Gamblers</li>
-                                <li>Madison Capitols</li>
-                                <li>Youngstown Phantoms</li>
+                                <li className="team-list">Aberdeen Wings</li>
+                                <li className="team-list">Austin Bruins</li>
+                                <li className="team-list">Chippewa Falls Steel</li>
+                                <li className="team-list">Johnstown Tomahawks</li>
+                                <li className="team-list">Kenai Brown Bears</li>
+                                <li className="team-list">MN Magicians</li>
+                                <li className="team-list">MN Wilderness</li>
+                                <li className="team-list">Minot Minotauros</li>
+                                <li className="team-list">NE Generals</li>
+                                <li className="team-list">Brookings Blizzard</li>
+                                <li className="team-list">Alexandria Blizzard</li>
+                                <li className="team-list">Bozeman Icedogs</li>
+                                <li className="team-list">Granite City Lumberjacks</li>
+                                <li className="team-list">Breezy Point Stars</li>
+                                <li className="team-list">New Ulm Steel</li>
+                                <li className="team-list">North Iowa Bulls</li>
+                                <li className="team-list">Rochester Grizzlies</li>
+                                <li className="team-list">MN Blue Ox</li>
+                                <li className="team-list">MN Mullets</li>
+                                <li className="team-list">Rum River</li>
+                                <li className="team-list">Steele County Blades</li>
+                                <li className="team-list">Hudson Havoc</li>
+                                <li className="team-list">Fort Frances Lakers</li>
+                                <li className="team-list">Tampa Bay Jr. Lightning</li>
+                                <li className="team-list">Oklahoma City Blazers</li>
+                                <li className="team-list">Thief River Falls Norskies</li>
+                                <li className="team-list">Swan Valley Stampeders</li>
+                                <li className="team-list">Steinbach Pistons</li>
+                                <li className="team-list">Winkler Flyers</li>
+                                <li className="team-list">Bonnyville Pontiacs</li>
+                                <li className="team-list">Green Bay Gamblers</li>
+                                <li className="team-list">Madison Capitols</li>
+                                <li className="team-list">Youngstown Phantoms</li>
                             </ul>
                         </div>
                     </div>
